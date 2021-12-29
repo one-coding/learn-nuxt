f6cbf7ff6c25b81d9483676daa1fe54b6fd72021
kimjaeone <ctonecoding@gmail.com>
2021-12-29 22:20:29 +0900

    learn-nuxt first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>홈 페이지</h1>
+    <p>넉스트 시작하기</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style></style>
---
+++ pages/product.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>상품 페이지</h1>
+    <ul>
+      <li>상품 1</li>
+      <li>상품 2</li>
+      <li>상풍 3</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductIndex',
+}
+</script>
+
+<style></style>
